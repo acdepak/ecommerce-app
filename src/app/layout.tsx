1217f1fb717b41c0f1d288ab9c049f6770272266
acdepak <acdepak@gmail.com>
2023-06-22 15:01:57 +0545

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import { AppFooter, AppHeader } from "@/components";
 import "./globals.css";
 
+export const metadata = {
+  title: "Slings for Babies",
+  description: "Get the best matching sling for you and your baby.",
+};
+
 export default function RootLayout({
   children,
 }: {
