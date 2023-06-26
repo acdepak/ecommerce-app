6c07fe93008cc50bafcb2b3014d09a05a95decb5
acdepak <acdepak@gmail.com>
2023-06-26 18:11:54 +0545

    added product section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ import {
   ScrollIcon,
   SecurityIcon,
 } from "@/icons";
+import { useWindowWidth } from "@react-hook/window-size";
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
 
 export default function Home() {
@@ -15,13 +18,12 @@ export default function Home() {
     slidesToScroll: 1,
     slidesToShow: 1,
     autoplay: false,
-    autoplaySpeed: 7000,
+    autoplaySpeed: 3000,
     fade: true,
     speed: 1000,
     lazyLoading: true,
-    dots: false,
+    dots: true,
     infinite: true,
-    arrows: false,
   };
 
   const heroImage = [
@@ -32,10 +34,10 @@ export default function Home() {
 
   return (
     <main className="flex flex-col ">
-      <div className="relative ">
+      <div className="relative h-[80vh] overflow-x-clip">
         <Slider {...settings}>
           {heroImage?.map(({ id, link, alt }) => (
-            <div className="h-[80vh]" key={id}>
+            <div className="relative h-[80vh]" key={id}>
               <Image
                 src={link}
                 alt={alt}
@@ -48,7 +50,7 @@ export default function Home() {
           ))}
         </Slider>
         <div className="absolute bottom-0 right-72 top-0 my-auto h-fit">
-          <div className="flex flex-col gap-5 text-white">
+          <div className="flex flex-col gap-5 text-cream">
             <p className="font font-josefin text-5xl font-semibold leading-[65px]">
               King's Day 2023!
             </p>
@@ -56,7 +58,7 @@ export default function Home() {
               Our market starts at noon. You receive a 25% discount with the
               code SFG500 on selected wraps and ring slings.
             </p>
-            <div className="flex items-center gap-3">
+            <Link href={"#"} className="flex items-center gap-3">
               <p className="font-josefin text-sm font-bold leading-[60px] tracking-wider">
                 SHOP ALL PRODUCTS
               </p>
@@ -65,7 +67,7 @@ export default function Home() {
                   <ArrowIcon />
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
         <div className="absolute bottom-0 top-0 my-auto h-fit w-full">
@@ -80,6 +82,7 @@ export default function Home() {
         </div>
       </div>
       <WorkFlowSection />
+      <ProductSection />
       Hello
     </main>
   );
@@ -107,18 +110,173 @@ const WorkFlowSection = () => {
     },
   ];
   return (
-    <div className="grid w-full grid-cols-3 bg-cream px-48 py-14">
+    <div className="grid w-full grid-cols-1 gap-y-5 bg-cream px-5 py-14 md:grid-cols-3 lg:px-48">
       {works.map((work) => (
-        <div key={work.id} className="flex flex-col items-center">
-          <div className="h-24 w-24 ">{work.icon}</div>
-          <p className="text-center font-josefin text-lg font-bold leading-10">
-            {work.title}
-          </p>
-          <p className="max-w-sm text-center font-mulish text-base leading-6">
-            {work.des}
-          </p>
+        <div
+          key={work.id}
+          className="flex flex-col items-center justify-center gap-1"
+        >
+          <div className="h-20 w-20 lg:h-24 lg:w-24">{work.icon}</div>
+          <div className="flex flex-col gap-2">
+            <p className="text-center font-josefin font-semibold lg:font-bold lg:leading-10">
+              {work.title}
+            </p>
+            <p className="max-w-sm text-center font-mulish text-sm leading-6">
+              {work.des}
+            </p>
+          </div>
         </div>
       ))}
     </div>
   );
 };
+
+const ProductSection = () => {
+  const [isMobile, setIsMobile] = useState(true);
+  const windowWidth = useWindowWidth();
+
+  useEffect(() => {
+    setIsMobile(windowWidth < 811);
+  }, [windowWidth]);
+
+  const collegeImg = [
+    {
+      id: 1,
+      link: "/assets/images/image 102.png",
+      linksm: "/assets/images/image 102.png",
+      alt: "Image1",
+      title: "Woven Wraps",
+      des: "Woven wraps for children provide extreme comfort of carrying a baby in many ways!",
+      position: "bottom",
+      col: 1,
+    },
+    {
+      id: 2,
+      link: "/assets/images/hero102.png",
+      linksm: "/assets/images/image 103.png",
+      alt: "Image1",
+      title: "Ring Sling",
+      des: "Ring Slings are simple to use baby carriers suitable for newborn to toddler.",
+      position: "center",
+      col: 2,
+    },
+    {
+      id: 3,
+      link: "/assets/images/image104.png",
+      linksm: "/assets/images/image 104.png",
+      alt: "Image1",
+      title: "Soft Structured Carrier",
+      des: "The soft structured baby carrier has its own structure with padding, fabrics, straps and other accessories.",
+      position: "left",
+      col: 2,
+    },
+    {
+      id: 4,
+      link: "/assets/images/image 105.png",
+      linksm: "/assets/images/image 105.png",
+      alt: "Image1",
+      title: "Accessories",
+      des: "Accessorize your Ergobaby Carrier with matching Infant Inserts, Weather Covers and more.",
+      position: "bottom",
+      col: 1,
+    },
+  ];
+  return (
+    <div className="">
+      <div className="grid grid-flow-row grid-cols-1 gap-5 px-5 pt-10 text-white md:grid-cols-2 lg:grid-cols-3 lg:px-48">
+        {collegeImg?.map((img, id) => (
+          <div
+            className={`relative h-[45vh] ${
+              img.col == 2 ? "col-span-1 lg:col-span-2 " : null
+            }`}
+            key={id}
+          >
+            <Image
+              src={isMobile ? img.linksm : img.link}
+              alt={img.alt}
+              fill
+              quality={100}
+              sizes="(max-width: 768px) 100vw"
+              className="object-cover "
+            />
+            <div className="absolute h-full w-full bg-black/20" />
+            <div
+              className={`absolute flex flex-col gap-2 ${
+                img.position == "bottom"
+                  ? "bottom-[10%] left-5 right-0 m-auto lg:left-0"
+                  : img.position == "center"
+                  ? "bottom-[10%] left-5 right-0 m-auto lg:right-20 lg:top-1/3 "
+                  : "bottom-[10%] left-5 right-0 m-auto lg:left-20 lg:top-1/3 "
+              }`}
+            >
+              <p className="font-josefin text-2xl font-semibold leading-10 lg:text-4xl">
+                {img.title}
+              </p>
+              <p className="max-w-sm font-mulish text-base leading-6">
+                {img.des}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center justify-center py-14">
+        <Link href={"#"} className="rounded-full border border-black p-3 px-5">
+          SHOP ALL PRODUCTS
+        </Link>
+      </div>
+    </div>
+  );
+};
+{
+  /* <div className="relative h-[400px]">
+          <Image
+            src="/assets/images/image 102.png"
+            alt="image1"
+            fill
+            quality={100}
+            sizes="(max-width: 768px) 100vw"
+            className="object-cover"
+          />
+          <div className="absolute">
+            <p>Woven Wraps</p>
+            <p>
+              Woven wraps for children provide extreme comfort of carrying a
+              baby in many ways!
+            </p>
+          </div>
+        </div>
+        <div className="relative col-span-2 h-[400px]">
+          <Image
+            src="/assets/images/hero102.png"
+            alt="image2"
+            fill
+            quality={100}
+            sizes="(max-width: 768px) 100vw"
+            className="object-cover"
+          />
+          <div className="absolute">
+            <p>Ring Sling</p>
+            <p>
+              Ring Slings are simple to use baby carriers suitable for newborn
+              to toddler.
+            </p>
+          </div>
+        </div>
+        <div className="relative col-span-2 h-[400px]">
+          <Image
+            src="/assets/images/image104.png"
+            alt="image3"
+            fill
+            quality={100}
+            sizes="(max-width: 768px) 100vw"
+            className="object-cover"
+          />
+          <div className="absolute">
+            <p>Soft Structured Carrier</p>
+            <p>
+              The soft structured baby carrier has its own structure with
+              padding, fabrics, straps and other accessories.{" "}
+            </p>
+          </div>
+        </div> */
+}
