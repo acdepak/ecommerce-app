b11ea6b8ebab9ef24a7a81b2d5088712defb8670
acdepak <acdepak@gmail.com>
2023-06-28 12:06:11 +0545

    updating the section

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -35,40 +35,42 @@ export const FeaturedSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center justify-end gap-10 bg-darkblue py-16 lg:gap-52 ">
-        {/*  */}
-        <div className="w-full bg-red md:pl-5 lg:h-[668px]">
-          <div className="relative h-[45vh] lg:h-full">
-            <Image
-              src="/assets/images/image 107.png"
-              alt="Red Sling"
-              fill
-              sizes="(max-width: 768px) 100vw"
-              className="object-cover "
-              quality={100}
-            />
+      <div className="bg-darkblue py-16 ">
+        <div className="flex flex-col items-center justify-center gap-5 bg-red md:flex-row lg:gap-52">
+          {/*  */}
+          <div className="w-full bg-sky md:w-1/2 md:pl-5 ">
+            <div className="relative h-[400px] lg:h-[668px] lg:w-[440px]">
+              <Image
+                src="/assets/images/image 107.png"
+                alt="Red Sling"
+                fill
+                sizes="(max-width: 768px) 100vw"
+                className="object-cover "
+                quality={100}
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-5 pl-10 text-white md:pr-10 lg:gap-8">
-          <h2 className="max-w-sm font-josefin text-3xl font-semibold lg:text-5xl lg:leading-[60px]">
-            Parents know parenthood best
-          </h2>
-          <p className="hidden  max-w-sm font-mulish text-base leading-7 lg:block lg:text-lg">
-            We&lsquo;re parents too, and we know how overwhelming it can feel to
-            shop for baby products. That&lsquo;s why SFB was born—to create a
-            no-brainer shopping experience for the next generation of parents
-            where you can buy quality products at a price that actually makes
-            sense.
-          </p>
-          <p className="max-w-sm font-mulish text-base leading-7 lg:hidden lg:text-lg">
-            Babywearing is an amazing parenting tool but it can be overwhelming
-            as a first time parent or caregiver. Our aim is to make your
-            babywearing journey easy and enjoyable!
-          </p>
-          <ShopAllProd url="#" varient="hero" />
+          <div className="flex flex-col gap-5 pl-10 text-white md:pr-10 lg:w-1/2 lg:gap-8 ">
+            <h2 className="max-w-sm font-josefin text-3xl font-semibold lg:text-5xl lg:leading-[60px]">
+              Parents know parenthood best
+            </h2>
+            <p className="hidden  max-w-sm font-mulish text-base leading-7 lg:block lg:text-lg">
+              We&lsquo;re parents too, and we know how overwhelming it can feel
+              to shop for baby products. That&lsquo;s why SFB was born—to create
+              a no-brainer shopping experience for the next generation of
+              parents where you can buy quality products at a price that
+              actually makes sense.
+            </p>
+            <p className="max-w-sm font-mulish text-base leading-7 lg:hidden lg:text-lg">
+              Babywearing is an amazing parenting tool but it can be
+              overwhelming as a first time parent or caregiver. Our aim is to
+              make your babywearing journey easy and enjoyable!
+            </p>
+            <ShopAllProd url="#" varient="hero" />
+          </div>
+          {/*  */}
         </div>
-        {/*  */}
       </div>
       {/*  */}
     </div>
