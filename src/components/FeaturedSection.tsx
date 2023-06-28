c829f117dea9969239f4b6b2f9c7493eaa983418
acdepak <acdepak@gmail.com>
2023-06-28 13:06:45 +0545

    completed featured section

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -5,7 +5,7 @@ export const FeaturedSection = () => {
   return (
     <div>
       {/*  */}
-      <div className="flex flex-col-reverse items-center justify-center gap-10 bg-cream py-16 md:flex-row lg:gap-52 ">
+      <div className="flex flex-col-reverse items-center justify-center gap-10 bg-cream py-16 md:flex-row lg:gap-52 lg:py-40 ">
         {/*  */}
         <div className="flex w-full justify-end pl-10 lg:w-1/3">
           <div className="flex flex-col items-start gap-5 lg:gap-8 ">
@@ -35,10 +35,10 @@ export const FeaturedSection = () => {
         </div>
       </div>
 
-      <div className="bg-darkblue py-16 ">
-        <div className="flex flex-col items-center justify-center gap-5 bg-red md:flex-row lg:gap-52">
+      <div className="bg-darkblue py-16 lg:py-5 ">
+        <div className="flex flex-col  items-center justify-center gap-5 md:flex-row lg:relative lg:bottom-20 lg:items-end lg:gap-52 lg:pl-40">
           {/*  */}
-          <div className="w-full bg-sky md:w-1/2 md:pl-5 ">
+          <div className="w-full md:w-1/2 md:pl-5 lg:flex lg:w-1/3 lg:justify-end">
             <div className="relative h-[400px] lg:h-[668px] lg:w-[440px]">
               <Image
                 src="/assets/images/image 107.png"
@@ -51,7 +51,7 @@ export const FeaturedSection = () => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-5 pl-10 text-white md:pr-10 lg:w-1/2 lg:gap-8 ">
+          <div className="flex flex-col gap-5 pl-10 text-white md:pr-10 lg:w-1/2 lg:gap-8 lg:pb-10 lg:pl-0 ">
             <h2 className="max-w-sm font-josefin text-3xl font-semibold lg:text-5xl lg:leading-[60px]">
               Parents know parenthood best
             </h2>
