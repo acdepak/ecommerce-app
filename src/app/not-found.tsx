12a75188c54a1e8d978dabbb26aad87401a6cf1a
acdepak <acdepak@gmail.com>
2023-07-04 17:52:17 +0545

    created 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,26 @@
 "use client";
+
+import { Typography } from "@/components/Typography";
+import { CryFaceIcon } from "@/icons";
+import Link from "next/link";
+
 export default function PageNotFound() {
-  return <div className="p-20 text-center text-5xl">PageNotFound</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-10 p-20">
+      <div className="h-20 w-20">
+        <CryFaceIcon />
+      </div>
+      <Typography varient="heading1" center className="max-w-4xl">
+        The page you are looking for does not exist&#40;
+      </Typography>
+      <Typography varient="body1">
+        We apologize for the inconvenience...
+      </Typography>
+      <Link href="/">
+        <Typography varient="body1" color="sky">
+          Home
+        </Typography>
+      </Link>
+    </div>
+  );
 }
