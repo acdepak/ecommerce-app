a9a58ce1d5f1a3986bd57a15dad9f25a65f2175a
acdepak <acdepak@gmail.com>
2023-07-05 18:04:04 +0545

    created gift page for component testing

---
+++ src/app/gift/page.tsx
@@ -0,0 +1,11 @@
+import { SocialSection } from "@/components";
+import { Products } from "@/components/Products";
+
+export default function page() {
+  return (
+    <div>
+      <Products>YOU MIGHT ALSO BE INTERESTED IN</Products>
+      <SocialSection />
+    </div>
+  );
+}
