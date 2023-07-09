aae5410c028def3c1f5c2879732d97381d033dfc
acdepak <acdepak@gmail.com>
2023-07-09 09:08:22 +0545

    updated css

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -6,17 +6,17 @@ import { Typography } from "./Typography";
 
 export const ProductsSection = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center overflow-clip">
       <Typography varient="heading5" center className="py-10 leading-7">
         {children}
       </Typography>
-      <div className="grid grid-flow-row grid-cols-1 items-center justify-center gap-x-5 gap-y-10 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-flow-row grid-cols-2 items-center justify-center gap-x-5 gap-y-2 md:grid-cols-2 md:gap-x-5 md:gap-y-10 lg:grid-cols-4">
         {product?.slice(0, 4).map((post, id) => (
           <div
             key={id}
-            className="flex w-fit flex-col items-center justify-center gap-5 "
+            className="flex w-[200px] flex-col items-start justify-center gap-5 md:w-fit "
           >
-            <div className="relative h-[541px] w-[360px] ">
+            <div className="relative h-[260px] w-[200px] md:h-[541px] md:w-[360px] ">
               <Image
                 src={post.src}
                 alt={post.title}
@@ -26,14 +26,15 @@ export const ProductsSection = ({ children }: { children: ReactNode }) => {
                 quality={100}
               />
             </div>
-            <div className="flex flex-col gap-2">
+
+            <div className="flex flex-col gap-2 pr-3 md:pr-0">
               <Typography varient="body">{post.title}</Typography>
-              <div className="flex justify-between">
+              <div className="flex flex-col md:flex-row md:justify-between">
                 <div className="flex items-center justify-start gap-1">
                   <Typography varient="body">From:</Typography>
                   <Typography varient="heading4">€{post.price}</Typography>
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center md:justify-center">
                   {post.stock ? (
                     <div className="flex items-center justify-center gap-2">
                       <div className="h-2 w-3 text-sky">
