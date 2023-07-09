b5d850e2c8250f0dcb38aab37a1544134fd52022
acdepak <acdepak@gmail.com>
2023-07-09 19:02:27 +0545

    completed static login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { BreadCrumb, Button, Typography } from "@/components";
+import { EyeClosedIcon, EyeOpenIcon } from "@/icons";
+import clsx from "clsx";
 import { useState } from "react";
 
 export default function page() {
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isHidden, setIsHidden] = useState(true);
   return (
     <>
       <BreadCrumb title="Sign-In" desc="" path="Home / Login" />
@@ -11,30 +14,52 @@ export default function page() {
         <Typography varient="heading5" center>
           SIGN-IN TO YOUR ACCOUNT:
         </Typography>
-        <div className="relative right-20 flex flex-col items-center gap-2">
-          <div className="flex items-center gap-5">
-            <label className="w-40 px-10">
+
+        <div className="relative flex flex-col items-center gap-2 lg:right-24">
+          <div className="flex flex-col items-start gap-5 lg:flex-row lg:items-center">
+            <label className="w-40 lg:px-10 ">
               <Typography varient="body">Email</Typography>
             </label>
+
             <input
               type="email"
               placeholder="your@mail.com"
               className="w-96 border-2 border-gray px-5 py-2 font-josefin text-xl text-dark focus:outline-none"
             />
           </div>
-          <div className="flex items-center gap-5">
-            <label className="w-40 px-10">
+
+          <div className="flex flex-col items-start gap-5 lg:flex-row lg:items-center">
+            <label className="w-40 lg:px-10">
               <Typography varient="body">Password</Typography>
             </label>
+
             <div className="flex w-96 items-center border-2 border-gray">
               <input
-                type="password"
+                type={isDisabled ? "password" : "text"}
                 className="w-full border-none px-5 py-2 font-josefin text-xl text-dark focus:outline-none"
               />
-              <div className="bg-red px-1">h</div>
+
+              <div
+                onClick={() => setIsDisabled(!isDisabled)}
+                className={clsx("h-4 w-5", {
+                  "hidden opacity-0 ": !isDisabled,
+                })}
+              >
+                <EyeClosedIcon />
+              </div>
+
+              <div
+                onClick={() => setIsDisabled(!isDisabled)}
+                className={clsx("h-4 w-5", {
+                  "hidden opacity-0 ": isDisabled,
+                })}
+              >
+                <EyeOpenIcon />
+              </div>
             </div>
           </div>
         </div>
+
         <div className="flex flex-col items-center gap-5 text-center">
           <Button varient="login" disabled={isDisabled}>
             SIGN IN
@@ -43,6 +68,7 @@ export default function page() {
             Forgot your password?
           </Typography>
         </div>
+
         <div>
           <Typography varient="heading5" center>
             SIGN-IN TO YOUR ACCOUNT:
