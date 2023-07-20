d501e9758d9a22fd2d00eacb4623a6474afacaa5
acdepak <acdepak@gmail.com>
2023-07-20 13:58:58 +0545

    added icons

---
+++ src/icons/DiamondIcon.tsx
@@ -0,0 +1,61 @@
+export const DiamondIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 512.00 512.00"
+      fill="#ffffff"
+      stroke="#ffffff"
+      stroke-width="0.00512"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke="#CCCCCC"
+        stroke-width="2.048"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        <polygon
+          fill="#84DBFF"
+          points="393.052,218.829 118.951,218.829 153.214,150.304 358.79,150.304 "
+        />
+        <polygon
+          fill="#31BAFD"
+          points="358.79,150.304 256.002,150.304 256.002,218.829 393.052,218.829 "
+        />
+        <polygon
+          fill="#B5F1F4"
+          points="118.951,218.829 256.002,390.142 256.002,390.141 256.002,218.829 "
+        />
+        <polygon
+          fill="#84DBFF"
+          points="256.002,218.829 256.002,390.141 393.052,218.829 "
+        />
+        <polygon
+          fill="#31BAFD"
+          points="188.514,218.829 256.002,390.142 323.489,218.829 "
+        />
+        <polygon
+          fill="#2B9ED8"
+          points="256.002,218.829 256.002,390.141 323.489,218.829 "
+        />
+        <g>
+          <polygon
+            fill="#B5F1F4"
+            points="358.79,150.304 323.489,218.829 256.002,150.304 188.514,218.829 153.214,150.304 "
+          />
+          <path
+            fill="#B5F1F4"
+            d="M256.002,150.304L256.002,150.304L256.002,150.304z"
+          />
+        </g>
+        <polygon
+          fill="#84DBFF"
+          points="358.79,150.304 256.002,150.304 256.002,150.304 323.489,218.829 "
+        />
+      </g>
+    </svg>
+  );
+};
---
+++ src/icons/PlatinumIcon.tsx
@@ -0,0 +1,52 @@
+export const PlatinumIcon = () => {
+  return (
+    <svg viewBox="0 0 512 512" fill="#000000">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <polygon
+          fill="#ddf3fc"
+          points="393.052,218.829 118.951,218.829 153.214,150.304 358.79,150.304 "
+        ></polygon>{" "}
+        <polygon
+          fill="#d1e6fa"
+          points="358.79,150.304 256.002,150.304 256.002,218.829 393.052,218.829 "
+        ></polygon>{" "}
+        <polygon
+          fill="#d2ecf7"
+          points="118.951,218.829 256.002,390.142 256.002,390.141 256.002,218.829 "
+        ></polygon>{" "}
+        <polygon
+          fill="#ddf3fc"
+          points="256.002,218.829 256.002,390.141 393.052,218.829 "
+        ></polygon>{" "}
+        <polygon
+          fill="#d1e6fa"
+          points="188.514,218.829 256.002,390.142 323.489,218.829 "
+        ></polygon>{" "}
+        <polygon
+          fill="#b6cfeb"
+          points="256.002,218.829 256.002,390.141 323.489,218.829 "
+        ></polygon>{" "}
+        <g>
+          <polygon
+            fill="#d2ecf7"
+            points="358.79,150.304 323.489,218.829 256.002,150.304 188.514,218.829 153.214,150.304 "
+          ></polygon>{" "}
+          <path
+            fill="#d2ecf7"
+            d="M256.002,150.304L256.002,150.304L256.002,150.304z"
+          ></path>{" "}
+        </g>{" "}
+        <polygon
+          fill="#ddf3fc"
+          points="358.79,150.304 256.002,150.304 256.002,150.304 323.489,218.829 "
+        ></polygon>{" "}
+      </g>
+    </svg>
+  );
+};
