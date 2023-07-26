8be9791e575f86081932947319a025dba081d3e4
acdepak <acdepak@gmail.com>
2023-07-26 17:37:09 +0545

    completed right side and description

--- src/components/ItemDisplaySection.tsx
+++ src/components/ItemDisplaySection.tsx
@@ -8,6 +8,7 @@ import {
   PlatinumIcon,
   ProhibitionIcon,
   SilverStarIcon,
+  TickIcon,
   TruckIcon,
 } from "@/icons";
 import { ShareIcon } from "@/icons/ShareIcon";
@@ -30,54 +31,56 @@ const GiftOptions = [
   {
     id: 1,
     title: "Bronze Box - value aprox. €85",
-    price: "85",
+    price: 85,
     reference: "YREVTSLPRO61",
+    stock: 10,
+    like: true,
     name: "Bronze",
     icon: <BronzeStarIcon />,
   },
   {
     id: 2,
     title: "Silver Box - value aprox. €140",
-    price: "€140",
+    price: 140,
     reference: "YREVTSLPRO62",
+    stock: 10,
+    like: true,
     name: "Silver",
     icon: <SilverStarIcon />,
   },
   {
     id: 3,
     title: "Gold Box - value aprox. €195",
-    price: "195",
+    price: 195,
     reference: "YREVTSLPRO63",
+    stock: 10,
+    like: true,
     name: "Gold",
     icon: <GoldStarIcon />,
   },
   {
     id: 4,
     title: "Platinum Box - value aprox. €295",
-    price: "295",
+    price: 295,
     reference: "YREVTSLPRO64",
+    stock: 10,
+    like: true,
     name: "Platinum",
     icon: <PlatinumIcon />,
   },
   {
     id: 5,
     title: "Diamond Box - value aprox. €350",
-    price: "350",
+    price: 350,
     reference: "YREVTSLPRO65",
+    stock: 10,
+    like: true,
     name: "Diamond",
     icon: <DiamondIcon />,
   },
 ];
 
-export const ItemDisplaySection = ({
-  path,
-  title,
-  price,
-}: {
-  path: string;
-  title: string;
-  price: string;
-}) => {
+export const ItemDisplaySection = () => {
   const TopSettingRef = createRef<Slider>();
 
   const TopSettings = {
@@ -112,8 +115,10 @@ export const ItemDisplaySection = ({
     option: SetStateAction<{
       id: number;
       title: string;
-      price: string;
+      price: number;
       reference: string;
+      stock: number;
+      like: boolean;
       name: string;
       icon: JSX.Element;
     }>
@@ -181,35 +186,46 @@ export const ItemDisplaySection = ({
 
         <div className="flex w-2/5 flex-col gap-5">
           <Typography varient="body1" color="grayhard">
-            {path}
+            Home / Mystery Box
           </Typography>
 
           <Typography varient="heading2" color="dark">
-            {title}
+            {`SFB Mystery box - ${selectedGift.name}`}
           </Typography>
 
-          <div className="flex w-full justify-between">
-            <div className="flex flex-col">
-              <Typography varient="social">€{price}</Typography>
+          <div className="flex w-3/4 justify-between">
+            <div className="flex flex-col items-start">
+              <Typography varient="social">€{selectedGift.price}</Typography>
               <Typography varient="body1" color="grayhard">
                 Tax Inc.
               </Typography>
             </div>
 
-            {/* out of Stock */}
-            <div>
-              <div className="flex items-center gap-2 ">
-                <div className="h-4 w-4 rotate-90 stroke-2 text-red">
-                  <ProhibitionIcon />
-                </div>
+            {/*Stocked / out of Stock */}
 
-                <Typography varient="body1" color="red">
-                  Out of Stock
+            <div className="">
+              <div className="flex items-center gap-2 ">
+                {selectedGift.stock == 0 ? (
+                  <div className="h-4 w-4 rotate-90 stroke-2 text-red">
+                    <ProhibitionIcon />
+                  </div>
+                ) : (
+                  <div className="h-4 w-4 stroke-2 text-sky">
+                    <TickIcon />
+                  </div>
+                )}
+
+                <Typography
+                  varient="body1"
+                  color={selectedGift.stock < 1 ? "red" : "sky"}
+                >
+                  {selectedGift.stock < 1 ? "Out of Stock" : "In Stock"}
                 </Typography>
               </div>
 
               <Typography varient="body">
-                Reference: <span className="font-semibold">YREVTSLPRO65</span>
+                Reference:{" "}
+                <span className="font-semibold">{selectedGift.reference}</span>
               </Typography>
             </div>
           </div>
@@ -240,7 +256,6 @@ export const ItemDisplaySection = ({
           <div className="w-2/3 ">
             <div
               onClick={showAllOptions}
-              //  onClick={() => setIsClick(!isClick)}
               className="flex items-center justify-between gap-5 border-2 border-grayhard px-5 py-3"
             >
               <div />
@@ -295,8 +310,12 @@ export const ItemDisplaySection = ({
             </div>
 
             {/* Button */}
-            <Link href="#">
-              <Button varient="product">NOTIFY WHEN AVAILABLE</Button>
+            <Link href={selectedGift.stock < 1 ? "/notify" : "/addtocart"}>
+              <Button varient="product">
+                {selectedGift.stock < 1
+                  ? "NOTIFY WHEN AVAILABLE"
+                  : "ADD TO CART"}
+              </Button>
             </Link>
 
             {/* Like */}
@@ -324,9 +343,58 @@ export const ItemDisplaySection = ({
       </div>
 
       {/* Description */}
-      <div>
-        <div className="flex items-center justify-center gap-10 ">
-          Description
+      <div className="flex flex-col items-center justify-center gap-5 py-10">
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col items-start gap-5 ">
+            <Typography
+              varient="heading4"
+              color="sky"
+              className="underline underline-offset-8"
+            >
+              Description
+            </Typography>
+
+            <Typography varient="heading3"> More about product</Typography>
+          </div>
+
+          <Typography varient="body">
+            Thank you for being interested in our Mystery Box!
+            <br /> Are you up for a surprise or maybe a challenge?
+          </Typography>
+
+          <Typography varient="body" className="max-w-4xl">
+            Our Mystery Boxes are for everyone, available in 5 different sizes.
+            Do you have a hard time choosing a wrap or color or are you a thrill
+            seeker who loves surprises? We are going to do best wee can to make
+            the nicest and most beautiful Mystery Box ever, fitted for you.
+            Order the Mystery Box you like, receive a e-mail with a form. By
+            filling out this form, we get to know you more and hear your
+            preferences. With this information, we hope to surprise you with a
+            box that will dfinitely be a match for you.
+          </Typography>
+
+          <Typography varient="body" className="max-w-4xl">
+            With the Mystery Box more valur than what you have paid is
+            guranteed!
+          </Typography>
+
+          <div className="flex flex-col items-start gap-2 ">
+            {GiftOptions?.map((option) => (
+              <div
+                key={option.id}
+                className="z-10 flex items-center gap-2 "
+                onClick={() => handleOptionSelect(option)}
+              >
+                <div className="h-10 w-10">{option.icon}</div>
+                {option.title}
+              </div>
+            ))}
+          </div>
+
+          <Typography varient="body" className="max-w-4xl">
+            Returning is possible, at your own costs. This has to be the whole
+            box, not parts of it.
+          </Typography>
         </div>
       </div>
     </div>
